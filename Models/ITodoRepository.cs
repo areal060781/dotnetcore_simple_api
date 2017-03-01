acf1fe509a758b48e1779a2017c62c38b3d49418
Aida Real <areal060781@gmail.com>
2017-03-01 16:33:41 -0500

    convert the interface as a repository class

--- Models/ITodoRepository.cs
+++ Models/ITodoRepository.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace TodoApi.Models
 {
-    interface ITodoRepository
+    public interface ITodoRepository
     {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(long key);
+        void Remove(long key);
+        void Update(TodoItem item);
     }
 }
