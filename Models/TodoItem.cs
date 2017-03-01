3769be1e6674980b646e619122235c080cc5d186
Aida Real <areal060781@gmail.com>
2017-03-01 16:29:06 -0500

    Add model structure

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TodoApi.Models
 {
     public class TodoItem
     {
-        public TodoItem()
-        {
-        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Key { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
