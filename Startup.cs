c90e5c234516923429469ab305119a0c930874d3
Aida Real <areal060781@gmail.com>
2017-03-01 16:46:06 -0500

    register the repository

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace TodoApi
 {
     public class Startup
@@ -27,8 +30,12 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
+
             // Add framework services.
             services.AddMvc();
+
+            services.AddSingleton<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
